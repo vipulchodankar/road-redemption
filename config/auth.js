6cdb6a971d6728e187e115364e65620b34e2eed3
Vipul Chodankar <vipul72c@gmail.com>
2019-09-28 00:13:45 +0530

    Auth check for dashboard

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+  ensureAuthenticated: function(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    req.flash("error_msg", "Please login to access your dashboard.");
+    res.redirect("/users/login");
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const { ensureAuthenticated } = require("../config/auth");
 
 router.get("/", (req, res) => res.render("welcome"));
 
-router.get("/dashboard", (req, res) => res.render("dashboard"));
+router.get("/dashboard", ensureAuthenticated, (req, res) =>
+  res.render("dashboard", {
+    name: req.user.name
+  })
+);
 
 router.get("/view", (req, res) => res.render("view"));
 
