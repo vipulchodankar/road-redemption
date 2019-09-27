b9715514a3a989787880ebe4ca76d612a2978072
Vipul Chodankar <vipul72c@gmail.com>
2019-09-28 00:03:54 +0530

    Logout implemented

--- routes/users.js
+++ routes/users.js
@@ -76,4 +76,11 @@ router.post("/login", (req, res, next) => {
   })(req, res, next);
 });
 
+// Logout Request
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success_msg", "You are logged out.");
+  res.redirect("/users/login");
+});
+
 module.exports = router;
